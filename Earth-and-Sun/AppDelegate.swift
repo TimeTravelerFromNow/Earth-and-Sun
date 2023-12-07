a4cf057fb64549f30e498026c85c4863e8e91912
SebD <sebastian.detering@gmail.com>
2023-12-07 14:26:01 -0800

    Initial Commit

---
+++ Earth-and-Sun/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Earth-and-Sun
+//
+//  Created by sebi d on 12/7/23.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
